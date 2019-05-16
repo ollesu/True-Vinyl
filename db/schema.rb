569382f37e0b45b3e6be4e7fac4f87d29c54493e
Aisa Mukukenova <mkaysa@gmail.com>
2019-05-16 15:47:34 +0100

    created mvc - 3 models, controllers and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_05_16_143614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "purchases", force: :cascade do |t|
+    t.bigint "vinyl_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "buyer_id"
+    t.index ["vinyl_id"], name: "index_purchases_on_vinyl_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "vinyls", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.float "price"
+    t.string "artist"
+    t.integer "year"
+    t.text "description"
+    t.string "condition"
+    t.boolean "sold", default: false
+    t.string "photo"
+    t.string "media_link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "seller_id"
+  end
+
+  add_foreign_key "purchases", "vinyls"
 end
